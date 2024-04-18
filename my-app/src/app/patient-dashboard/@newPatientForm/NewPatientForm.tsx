c381d7c7bf9cec4ea938a51d1ff8165f71929050
Justin Morales <bainnix@gmail.com>
2024-04-18 16:10:08 -0400

    practice form

--- my-app/src/app/patient-dashboard/@newPatientForm/NewPatientForm.tsx
+++ my-app/src/app/patient-dashboard/@newPatientForm/NewPatientForm.tsx
@@ -1,8 +1,81 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  firstName: z.string().min(2, {
+    message: 'First name must be at least 2 characters.'
+  }),
+  lastName: z.string().min(2, {
+    message: 'Last name must be at least 2 characters.'
+  })
+})
+
 export default function NewPatientForm () {
-    return (
-        <>
-        <div>New Patient Form</div>
-        <div></div>
-        </>
-    )
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        firstName: '',
+        lastName: ''
+    }
+  })
+  function onSubmit (values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='firstName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First Name</FormLabel>
+              <FormControl>
+                <Input placeholder='First Name' {...field} />
+              </FormControl>
+              <FormDescription>
+                Enter your first name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='lastName'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder='Last Name' {...field} />
+              </FormControl>
+              <FormDescription>
+                Enter your last name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  )
 }
--- my-app/src/app/patient-dashboard/layout.tsx
+++ my-app/src/app/patient-dashboard/layout.tsx
@@ -1,17 +1,20 @@
 export default function PatientDashboard ({
   children,
   patientTable,
+  newPatientForm,
 
 }: {
   children: React.ReactNode,
-  patientTable: React.ReactNode
+  patientTable: React.ReactNode,
+  newPatientForm: React.ReactNode
+  
 
 }) {
   return (
     <>
       <div> {children}</div>
       <div>{patientTable}</div>
-
+      <div>{newPatientForm}</div>
     </>
   )
 }
